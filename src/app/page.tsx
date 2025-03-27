adb3e4e2368fa6ab9dac33832339499f9ed33bc9
pivanrios <pipr96@gmail.com>
2025-03-26 22:33:07 -0700

    create and add Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 import Link from "next/link";
+import Hero from "../../components/Hero";
 
 
 export default function Home() {
   return (
    <>
-   <main>
+   <main className=" relative bg-black flex justify-center items-center">
     <div>
       {//navbar
-      }<div className="flex flex-row justify-between p-3">
-        <h2>Pivanrios</h2>
+      }<div className="flex flex-row p-3">
+        <h2 className=" italic text-white">Pivanrios</h2>
         <nav>
-          <ul className="flex flex-row justify-center space-x-1">
+          <ul className="flex flex-row justify-center space-x-1 text-white">
             <Link href={"/"}>Home</Link>
             <Link href={"/"}>About</Link>
             <Link href={"/"}>Connect</Link>
@@ -18,6 +19,7 @@ export default function Home() {
         </nav>
       </div>
 
+      <Hero/>
       
     </div>
    </main>
