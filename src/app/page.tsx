2653e6fac04df974764c7ebea66cd26ac11fe7c0
pivanrios <pipr96@gmail.com>
2025-03-26 22:04:28 -0700

    add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 
 export default function Home() {
@@ -5,10 +6,19 @@ export default function Home() {
    <>
    <main>
     <div>
-      <h1>Pivanrios Portfolio</h1>
-      <div>
-        
+      {//navbar
+      }<div className="flex flex-row justify-between p-3">
+        <h2>Pivanrios</h2>
+        <nav>
+          <ul className="flex flex-row justify-center space-x-1">
+            <Link href={"/"}>Home</Link>
+            <Link href={"/"}>About</Link>
+            <Link href={"/"}>Connect</Link>
+          </ul>
+        </nav>
       </div>
+
+      
     </div>
    </main>
    </>
