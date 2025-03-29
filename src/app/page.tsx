d3bf87287852084e6dfdd50d0a0b9682db9d0a96
pivanrios <pipr96@gmail.com>
2025-03-28 21:52:52 -0700

    strech navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,23 @@ import Toggle from "../../components/ui/Toggle";
 export default function Home() {
   return (
    <>
-   <main className=" relative flex justify-center items-center">
+   <main className=" relative flex flex-col">
     <div>
       {//navbar
-      }<div className="flex flex-row p-3">
+      }<div className="flex flex-row p-1 bg-amber-200 items-baseline justify-around min-w-full">
+        
         <h2 className=" italic ">Pivanrios</h2>
+
         <nav>
-          <ul className="flex flex-row justify-center space-x-1 ">
+          <ul className="flex flex-row xl:flex-col">
             <Link href={"/"}>Home</Link>
             <Link href={"/"}>About</Link>
             <Link href={"/"}>Connect</Link>
           </ul>
         </nav>
+
         <Toggle/>
+
       </div>
 
       <Hero/>
